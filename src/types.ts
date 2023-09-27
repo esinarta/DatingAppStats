6d63f9c3f59b7d96876e0f7a0a57e1bc610790a0
Estevan Sinarta <estevan_sinarta@yahoo.ca>
2023-09-26 20:59:30 -0700

    Add types for Hinge events

--- src/types.ts
+++ src/types.ts
@@ -1,9 +1,34 @@
-export type MatchStats  = {
-  total: number;
-  yes: number;
-  no: number;
-  matches: number;
-  chats: number;
-  longestChat: number;
-  avgChatLength: number;
+export interface HingeEvent {
+  type: "block" | "chat" | "like" | "match" | "we_met";
+  timestamp: string;
 }
+
+export interface HingeBlockEvent extends HingeEvent {
+  type: "block";
+  block_type: string;
+}
+
+export interface HingeChatEvent extends HingeEvent {
+  type: "chat";
+  body: string;
+}
+
+export interface HingeWeMetEvent extends HingeEvent {
+  type: "we_met";
+  did_meet_subject?: boolean;
+  was_my_type?: boolean;
+}
+
+export type HingeStats = {
+  matchStats: {
+    seen: number;
+    yes: number;
+    no: number;
+    matches: number;
+  };
+  chatStats: {
+    chats: number;
+    longestChat: number;
+    avgChatLength: number;
+  };
+};
