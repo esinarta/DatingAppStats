d285b0ab418674ff1a72526ae2592f86a49dd7f8
Estevan Sinarta <estevan_sinarta@yahoo.ca>
2023-09-26 20:59:44 -0700

    Create parseHingeData helper

---
+++ src/utils/parseHingeData.ts
@@ -0,0 +1,45 @@
+import { HingeEvent } from "@/types";
+
+const parseMatchData = (events: HingeEvent[]) => {
+  const seen = events.length;
+
+  const matches = events.filter((item: any) => item.hasOwnProperty("match"));
+
+  const yes = events.filter((item: any) => item.hasOwnProperty("like"));
+
+  const no = events.filter((item: any) => item.hasOwnProperty("block"));
+
+  return {
+    seen,
+    yes: yes.length,
+    no: no.length,
+    matches: matches.length,
+  };
+};
+
+const parseChatData = (events: HingeEvent[]) => {
+  const chats = events.filter((item: any) => item.hasOwnProperty("chats"));
+
+  const longestChat = chats.reduce(
+    (acc: number, curr: any) =>
+      curr.chats.length > acc ? curr.chats.length : acc,
+    0
+  );
+
+  const avgChatLength =
+    chats.reduce((acc: number, curr: any) => acc + curr.chats.length, 0) /
+    chats.length;
+
+  return {
+    chats: chats.length,
+    longestChat,
+    avgChatLength,
+  };
+};
+
+export const parseHingeData = (events: HingeEvent[]) => {
+  return {
+    matchStats: parseMatchData(events),
+    chatStats: parseChatData(events),
+  };
+};
